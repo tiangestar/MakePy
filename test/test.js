deb4b7bf7880ecc818117e8c9d534f6f028c3691
tiange <1139496978@qq.com>
2015-06-05 21:59:27 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /**
  * Created by aminzone on 2015-06-05.
  */
-var makePy = require('../src/makepy');
+var tiangestar = require('../src/makepy');
 var str = "开个空间";
-var result = makePy.makePy(str)
+var result = tiangestar.makePy(str)
 console.log(result[0])
